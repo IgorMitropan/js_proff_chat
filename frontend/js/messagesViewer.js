fadc5c5d654376f38524016a1d14c7e857a16d9c
IgorMitropan <mitropan2205@gmail.com>
2016-05-11 17:59:42 +0300

    little bug with time was fixed

--- frontend/js/messagesViewer.js
+++ frontend/js/messagesViewer.js
@@ -27,7 +27,7 @@ export default class MessagesViewer extends Component{
 
         let minutes = date.getMinutes();
         if (minutes < 10) {
-            minutes = '0'+ hours;
+            minutes = '0'+ minutes;
         }
 
         return  month + '/' + day + '/' + year + ' at ' + hours + ':' + minutes;
